ce0951abf05fb7a7e89d930d76180c647603ad20
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-10-18 17:29:37 +0200

    added fragments and test controller

--- src/main/java/dev/naimsulejmani/webdevelopmentgrupi3/controllers/TestController.java
+++ src/main/java/dev/naimsulejmani/webdevelopmentgrupi3/controllers/TestController.java
@@ -1,4 +1,24 @@
 package dev.naimsulejmani.webdevelopmentgrupi3.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    //generate about page
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    //generate contact page
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
 }
